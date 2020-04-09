5f10b96600e785ea4e4dc5ca655626def89ddbbb
Lenni Korhonen <lenni.korhonen2@gmail.com>
2020-04-09 13:55:58 +0300

    postgres bug fixes

--- src/main/java/com/example/cateringProject/CateringProjectApplication.java
+++ src/main/java/com/example/cateringProject/CateringProjectApplication.java
@@ -39,26 +39,26 @@ public class CateringProjectApplication {
 			System.out.println("Kategoriat lisätty");
 			
 			log.info("save a couple of products");
-			repository.save(new Product("Pulla", 2.00, crepository.findByName("Kaffepaku").get(0)));
+			/*repository.save(new Product("Pulla", 2.00, crepository.findByName("Kaffepaku").get(0)));
 			repository.save(new Product("Kakku pala", 3.50, crepository.findByName("Catering").get(0)));
 			repository.save(new Product("Pasteija", 2.00, crepository.findByName("Hyvät ideat").get(0)));
 			repository.save(new Product("Catering paketti 1", 75, crepository.findByName("Catering").get(0)));
 			repository.save(new Product("Catering paketti 2", 125, crepository.findByName("Catering").get(0)));
-			repository.save(new Product("Catering paketti 3", 140, crepository.findByName("Catering").get(0)));
+			repository.save(new Product("Catering paketti 3", 140, crepository.findByName("Catering").get(0)));*/
 			
 			System.out.println("Tuotteet lisätty");
 			
 			log.info("save couple example customers");
-			curepository.save(new Customer("Faijan leivät", "Matti" , "Meikäläinen", "matti.meikalainen@faija.fi",
+			/*curepository.save(new Customer("Faijan leivät", "Matti" , "Meikäläinen", "matti.meikalainen@faija.fi",
 					"Leipätie 5", 1, repository.findByName("Catering paketti 3").get(0)));
 			curepository.save(new Customer("Mutsin kakut", "Maija" , "Moilanen", "maija.moilanen@mutsi.fi",
-					"Kakkutie 7", 1, repository.findByName("Catering paketti 1").get(0)));
+					"Kakkutie 7", 1, repository.findByName("Catering paketti 1").get(0)));*/
 			
 			log.info("save a couple of users");
-			urepository.save(new EndUser("user", "$2y$12$UY92f4lCumIk90xY6Z9ZMODKPmm8sOG5cVfAnNEeMEocvTv0oMN7C",
+			/*urepository.save(new EndUser("user", "$2y$12$UY92f4lCumIk90xY6Z9ZMODKPmm8sOG5cVfAnNEeMEocvTv0oMN7C",
 					"USER"));
 			urepository.save(new EndUser("admin", "$2y$12$5piFyCJx6vDwNTDuk.6FWe0rLgzz0C1sD2h/RsqS1/YCDM7rpGMwW",
-					"ADMIN"));
+					"ADMIN"));*/
 			
 			System.out.println("Käyttäjät lisätty");
 			
