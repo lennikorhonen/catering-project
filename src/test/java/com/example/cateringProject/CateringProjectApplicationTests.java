9c5f84f0c74d6c5bfc27e68d977ea7ce802f89ed
Lenni Korhonen <lenni.korhonen2@gmail.com>
2020-04-16 15:25:59 +0300

    bug fix

--- src/test/java/com/example/cateringProject/CateringProjectApplicationTests.java
+++ src/test/java/com/example/cateringProject/CateringProjectApplicationTests.java
@@ -1,13 +1,10 @@
 package com.example.cateringProject;
 
 
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class CateringProjectApplicationTests {
 
